a1e75716131bd15ae91e8cdec0b98c5401c84a0d
AbriGut <abiizgnunez@gmail.com>
2022-11-14 08:07:57 -0600

    Acomodar Dashboard

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.app')
-
 @section('template_title')
-    Product
+    Productos
 @endsection
-
 @section('content')
+<br><br><br><br>
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
                 <div class="card">
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
-
                             <span id="card_title">
                                 {{ __('Product') }}
                             </span>
-
                              <div class="float-right">
                                 <a href="{{ route('products.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
                                   {{ __('Create New') }}
